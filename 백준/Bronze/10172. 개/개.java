20e9decc0dc974bca95be31c034bbf2ee7101d97
변미현 <81346092+MeehyunByeon@users.noreply.github.com>
2024-05-24 23:08:25 +0900

    [Bronze V] Title: 개, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String args[]) {
+      System.out.format("|%c_/|\n",92);
+      System.out.println("|q p|   /}");
+      System.out.format("( 0 )%c%c%c%c\n",34,34,34,92);
+      System.out.format("|%c^%c`    |\n",34,34,92);
+      System.out.format("||_/=%c%c__|\n",92,92);
+    }
+}
