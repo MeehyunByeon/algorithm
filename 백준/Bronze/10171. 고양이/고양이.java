ae3d88f445b12da4bf7c7918b5ed51c3996f341c
변미현 <81346092+MeehyunByeon@users.noreply.github.com>
2024-07-08 21:00:06 +0900

    [Bronze V] Title: 고양이, Time: 140 ms, Memory: 14444 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String args[]) {
+      System.out.format("%c    /%c\n",92,92);
+      System.out.println(" )  ( ')");
+      System.out.println("(  /  )");
+      System.out.format(" %c(__)|",92);
+    }
+}
