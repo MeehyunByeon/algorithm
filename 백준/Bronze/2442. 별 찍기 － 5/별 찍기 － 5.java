f14327061f72869b2fde999604189d2865895d7d
변미현 <81346092+MeehyunByeon@users.noreply.github.com>
2024-09-17 23:03:48 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 404 ms, Memory: 24008 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int k =n;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = k; j > 1; j--) 
+            {
+                System.out.print(" ");
+            }
+            for(int o = 0; o < 2*(i+1)-1; o++)
+            {
+                System.out.print("*");
+            }
+            k--;
+            System.out.print("\n");
+        }
+    }
+}
