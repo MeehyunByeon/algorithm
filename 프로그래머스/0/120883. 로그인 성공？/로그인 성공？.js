3e6866fab7dd014dcf32e9d365c1d877224e55a2
변미현 <81346092+MeehyunByeon@users.noreply.github.com>
2024-11-26 23:27:41 +0900

    [level 0] Title: 로그인 성공?, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,8 +1,6 @@
 function solution(id_pw, db) {
     let answer = "fail";
-    for(let [id, pw] of db) {
-        if(id == id_pw[0] && pw == id_pw[1]) answer = "login";
-        else if(id == id_pw[0] && pw != id_pw[1]) answer = "wrong pw";
-    }
+    for(let [id, pw] of db)
+        if(id==id_pw[0]) answer = pw==id_pw[1] ? "login" : "wrong pw";
     return answer;
 }
