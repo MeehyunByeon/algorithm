c277d6b5883f553e0311c5720a45775542f5bd55
변미현 <81346092+MeehyunByeon@users.noreply.github.com>
2024-12-22 14:43:49 +0900

    [level 0] Title: A 강조하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -1,8 +1,5 @@
 function solution(myString) {
     let answer = '';
-    for(let i of myString) {
-        if(i == 'a') answer += i.toUpperCase();
-        else answer += i == 'A' ? 'A' : i.toLowerCase();
-    }
+    for(let i of myString) answer += i=='a' ? i.toUpperCase() : (i =='A' ? 'A' : i.toLowerCase());
     return answer;
 }
