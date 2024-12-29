5d06c4301cf16fcec1952fed3786837e8bb9ab22
변미현 <81346092+MeehyunByeon@users.noreply.github.com>
2024-12-30 07:07:08 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.js
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.js
@@ -1,6 +1,5 @@
 function solution(num_list, n) {
     let answer = [];
-    let len = num_list.length;
-    for(let i = n-1; i < len; i++) answer.push(num_list[i]);
+    for(let i = n-1; i < num_list.length; i++) answer.push(num_list[i]);
     return answer;
 }
