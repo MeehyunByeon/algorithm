20063b5055c7b8bdbe0ba8f1acc758099364bc29
변미현 <81346092+MeehyunByeon@users.noreply.github.com>
2025-01-09 20:27:58 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,9 +1,6 @@
 function solution(num_list) {
     let answer = [0, 0];
     let len = num_list.length;
-    for(let i = 0; i < len; i++) {
-        if(num_list[i]%2==0) answer[0]++;
-        else answer[1]++;
-    }
+    for(let i = 0; i < len; i++) num_list[i]%2==0 ? answer[0]++ : answer[1]++;
     return answer;
 }
